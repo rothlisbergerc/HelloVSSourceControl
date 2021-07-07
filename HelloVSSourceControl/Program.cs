570045b4c58214642e2b56bb71cb8fa85ae54477
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-07 10:57:29 -0700

    Changed welcome message

--- HelloVSSourceControl/Program.cs
+++ HelloVSSourceControl/Program.cs
@@ -10,7 +10,7 @@ namespace HelloVSSourceControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the show!");
         }
     }
 }
