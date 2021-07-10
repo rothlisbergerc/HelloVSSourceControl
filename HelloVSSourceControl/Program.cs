9ecd18652f19dbe2fcf75ea3b244f2437fce6120
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-10 08:46:49 -0700

    Implemented displaying in stock vehicles from DB

--- HelloVSSourceControl/Program.cs
+++ HelloVSSourceControl/Program.cs
@@ -11,8 +11,29 @@ namespace HelloVSSourceControl
         static void Main()
         {
             Console.WriteLine("Welcome to the show!");
+            Console.WriteLine();
+
+            DisplayInStockCars();
+
+            Console.WriteLine();
             Console.WriteLine("Press a key to end");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Displays a list of in stock cars, in alphabetical order.
+        /// </summary>
+        static void DisplayInStockCars()
+        {
+            // Not pulling from an actual database yet...
+            List<string> inStockCars = new List<string>();
+            inStockCars.Add("Honda Prelude - Qty: 5");
+            inStockCars.Add("Kia Forte - Qty: 10");
+
+            foreach (string currCar in inStockCars)
+            {
+                Console.WriteLine(currCar);
+            }
         }
     }
 }
